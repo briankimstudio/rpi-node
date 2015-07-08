16d594f490ae8c77c9693a79a3e8398d8ce63640
Brian <tskim@newbook.local>
2015-07-07 22:04:32 -0500

    add install-node.sh

---
+++ install-node.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Script to install Node on Raspberry Pi
+#
+
+NODE_TMP="tmp"
+NODE_VER="node-v0.12.6"
+
+if [ -d "$NODE_TMP" ]; then
+	echo "cd $NODE_TMP..."
+	cd $NODE_TMP
+else
+	echo "Creating temporary directory..."
+	mkdir $NODE_TMP 
+	cd $NODE_TMP
+fi
+
+echo "Downloading Node..."
+wget https://nodejs.org/dist/latest/$NODE_VER.tar.gz
+echo "Uncompressing $NODE_VER.tar.gz"
+tar -xvf $NODE_VER.tar.gz
+cd $NODE_VER
+echo "Configuring..."
+./configure
+echo "Compiling..."
+make
+echo "Installing..."
+sudo make install
