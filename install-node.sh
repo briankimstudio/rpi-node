30efea75d9760a9383502688c20f3ffb44a77e12
Brian <tskim@newbook.local>
2015-07-08 00:10:29 -0500

    add error check

--- install-node.sh
+++ install-node.sh
@@ -6,25 +6,40 @@
 NODE_TMP="tmp"
 NODE_VER="node-v0.12.6"
 
+downloading() {
+  echo "Downloading Node..."
+  wget https://nodejs.org/dist/latest/$NODE_VER.tar.gz
+}
+
+uncompressing() {
+  echo "Uncompressing $NODE_VER.tar.gz"
+  tar -xvf $NODE_VER.tar.gz
+}
+
 if [ -d "$NODE_TMP" ]; then
 	echo "cd $NODE_TMP..."
 	cd $NODE_TMP
 else
 	echo "Creating temporary directory..."
-	mkdir $NODE_TMP 
+	mkdir $NODE_TMP
 	cd $NODE_TMP
 fi
 
-echo "Downloading Node..."
-wget https://nodejs.org/dist/latest/$NODE_VER.tar.gz
-echo "Uncompressing $NODE_VER.tar.gz"
-tar -xvf $NODE_VER.tar.gz
+if [ ! -f $NODE_VER.tar.gz ]; then
+  downloading
+fi
+
+uncompressing
+
+if [ $? -ne 0 ]; then
+  echo "$NODE_VER.tar.gz is corrupted..."
+  rm $NODE_VER.tar.gz
+  downloading
+  uncompressing
+fi
+
 cd $NODE_VER
-echo "Configuring..."
-./configure
-echo "Compiling..."
-make
-echo "Installing..."
-sudo make install
-echo "Checking version"
-node -v
+echo "Configuring...";./configure
+echo "Compiling...";make
+echo "Installing...";sudo make install
+echo "Checking version";node -v
